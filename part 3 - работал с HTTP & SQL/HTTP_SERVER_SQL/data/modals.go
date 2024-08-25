eb7a9a86a99610a23ca877e0405bdf5ad96025ed
Iliya Brook <iliyabrook1987@gmail.com>
2024-08-25 12:21:32 +0300

    HTTP server SQL

--- part 3 - работал с HTTP & SQL/HTTP_SERVER_SQL/data/modals.go
+++ part 3 - работал с HTTP & SQL/HTTP_SERVER_SQL/data/modals.go
@@ -3,23 +3,23 @@ package data
 import "time"
 
 type DbStruct struct {
-	Users  []UserStruct  `json:"users"`
-	Orders []OrderStruct `json:"orders"`
+	Users  []UserStruct  `json:"users" db:"users"`
+	Orders []OrderStruct `json:"orders" db:"orders"`
 }
 
 type UserStruct struct {
-	ID           int       `json:"id"`
-	Name         string    `json:"name"`
-	Email        string    `json:"email"`
-	Password     string    `json:"password"`
-	RegisteredAt time.Time `json:"registered_at"`
+	ID           int       `json:"id" db:"id"`
+	Name         string    `json:"name" db:"name"`
+	Email        string    `json:"email" db:"email"`
+	Password     string    `json:"password" db:"password"`
+	RegisteredAt time.Time `json:"registered_at" db:"registered_at"`
 }
 
 type OrderStruct struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name"`
-	Quantity    int     `json:"quantity"`
-	Price       float64 `json:"price"`
-	Description string  `json:"description"`
-	UserID      int     `json:"user_id"`
+	ID          int     `json:"id" db:"id"`
+	Name        string  `json:"name" db:"name"`
+	Quantity    int     `json:"quantity" db:"quantity"`
+	Price       float64 `json:"price" db:"price"`
+	Description string  `json:"description" db:"description"`
+	UserID      int     `json:"user_id" db:"user_id"`
 }
--- part 3 - работал с HTTP & SQL/HTTP_SERVER_SQL/handlers/users/get_users.go
+++ part 3 - работал с HTTP & SQL/HTTP_SERVER_SQL/handlers/users/get_users.go
@@ -3,61 +3,50 @@ package users
 import (
 	"HTTP_SERVER/data"
 	"HTTP_SERVER/utils"
-	"database/sql"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 )
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	userId := r.URL.Query().Get("id")
-	var resp []byte
-	var rows *sql.Rows
 	var err error
-	var users []data.UserStruct
-	var user data.UserStruct
+	var resp []byte
 
 	if userId == "" {
-		rows, err = data.DB.Query("SELECT * FROM users")
+		var users []data.UserStruct
+
+		err = data.DB.Select(&users, "SELECT id, name, email, password, registered_at FROM users")
 		if err != nil {
-			fmt.Println(err)
+			utils.HandleServerError(err, w, "Failed to load users", "test")
+			return
 		}
-		defer rows.Close()
-		for rows.Next() {
-			err = rows.Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.RegisteredAt)
-			if err != nil {
-				fmt.Printf("Error scanning row users: %v\n", err)
-			}
-			users = append(users, user)
-		}
+
 		data.DbInst.Users = users
-		utils.HandleServerError(err, w, "Failed to load database")
 		resp, err = json.Marshal(data.DbInst.Users)
 	} else {
-		id, _ := strconv.Atoi(userId)
-		rows, err = data.DB.Query("SELECT * FROM users WHERE id=$1", id)
-
+		id, err := strconv.Atoi(userId)
 		if err != nil {
-			fmt.Println("Get users error:", err)
-		}
-		defer rows.Close()
-
-		if !rows.Next() {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("User not found"))
+			http.Error(w, "Invalid user ID", http.StatusBadRequest)
 			return
-		} else {
-			err = rows.Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.RegisteredAt)
-			if err != nil {
-				fmt.Printf("Error scanning row users: %v\n", err)
-			}
-			fmt.Println("log found user:", user)
-			resp, err = json.Marshal(user)
 		}
+
+		var user data.UserStruct
+
+		err = data.DB.Get(&user, "SELECT id, name, email, password, registered_at FROM users WHERE id=$1", id)
+		if err != nil {
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+		}
+
+		resp, err = json.Marshal(user)
 	}
 
-	utils.HandleServerError(err, w, "Failed to marshal users data")
+	if err != nil {
+		utils.HandleServerError(err, w, "Failed to marshal users data")
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	_, writeUserErr := w.Write(resp)
